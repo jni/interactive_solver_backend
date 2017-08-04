ef3e22bc5a06de344f4fa0b591749a0af74dcd89
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2017-08-04 11:15:43 -0400

    Raw structure

---
+++ python_backend/solver_backend.py
@@ -0,0 +1,8 @@
+from __future__ import division, print_function
+
+import zmq
+from solver_utils import preprocess, solve_multicut
+
+
+def solver_backend():
+    pass
---
+++ python_backend/solver_utils.py
@@ -0,0 +1,21 @@
+from __future__ import division, print_function
+
+import nifty
+import nifty.graph.rag as nrag
+import nifty.graph.optimization.multicut as nmc
+
+
+def rag():
+    pass
+
+
+def features():
+    pass
+
+
+def preprocess():
+    pass
+
+
+def solve_multicut(uv_ids, weights):
+    pass
