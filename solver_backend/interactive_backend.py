01781303428b4836308609ae6d58727b2bd00b61
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2017-08-28 14:24:05 -0400

    setting edge weigths between clusters (not tested yet)

--- solver_backend/interactive_backend.py
+++ solver_backend/interactive_backend.py
@@ -1,8 +1,10 @@
 from __future__ import division, print_function
 
 import zmq
+
 from .solver_utils import preprocess_with_random_forest, preprocess_with_simple_statistics
 from .solver_utils import solve_multicut, node_result_to_edge_result
+from .utils import cartesian_product
 
 
 # this sets the corresponding values in costs in-place
@@ -10,6 +12,15 @@ def set_costs_from_uv_ids(graph, costs, uv_pairs, value):
     edge_ids = graph.findEdges(uv_pairs)
     edge_ids = edge_ids[edge_ids != -1]
     costs[edge_ids] = value
+
+
+def set_costs_from_cluster_ids(graph, costs, node_labeling, cluster_u, cluster_v, value):
+    # find the node-ids in the original graph belonging to the cluster-ids
+    node_ids_u = np.where(node_labeling == cluster_u)[0]
+    node_ids_v = np.where(node_labeling == cluster_v)[0]
+    # build all possible pairs and set values (TODO is there a cheaper way to do this)
+    potential_uvs = cartesian_product(node_ids_u, node_ids_v)
+    set_costs_from_uv_ids(graph, costs, potential_uvs, value)
 
 
 def solver_backend():
--- solver_backend/utils.py
+++ solver_backend/utils.py
@@ -1,4 +1,5 @@
 import h5py
+import numpy as np
 
 
 def read_hdf5(path, key):
@@ -13,3 +14,14 @@ def write_hdf5(data, path, key, compression=None):
             f.create_dataset(key, data=data)
         else:
             f.create_dataset(key, data=data, compression=compression)
+
+
+# cartesian product for arbitrary number of arrays.
+# cf. https://stackoverflow.com/questions/11144513/numpy-cartesian-product-of-x-and-y-array-points-into-single-array-of-2d-points/11146645#11146645
+def cartesian_product(*arrays):
+    la = len(arrays)
+    dtype = np.result_type(*arrays)
+    arr = np.empty([len(a) for a in arrays] + [la], dtype=dtype)
+    for i, a in enumerate(np.ix_(*arrays)):
+        arr[...,i] = a
+    return arr.reshape(-1, la)
