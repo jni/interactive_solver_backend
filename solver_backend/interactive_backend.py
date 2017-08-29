de4537a6777f002d13fe3651992f25ce0dde9996
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2017-08-29 17:53:29 -0400

    Add first version of solver server with unit test

--- solver_backend/interactive_backend.py
+++ solver_backend/interactive_backend.py
@@ -1,17 +1,140 @@
 from __future__ import division, print_function
 
+import nifty
+
+import numpy as np
+
+import struct
+
+import threading
+
 import zmq
 
 from .solver_utils import preprocess_with_random_forest, preprocess_with_simple_statistics
 from .solver_utils import solve_multicut, node_result_to_edge_result
 from .utils import cartesian_product
 
+class SolverServer( object ):
+
+	def __init__( self, graph, costs, address, initial_solution, repulsive_cost=-100, attractive_cost=+100 ):
+
+	    super( SolverServer, self ).__init__()
+
+	    self.graph          = graph
+	    self.costs          = np.copy( costs )
+	    self.initial_costs  = costs
+	    self.address        = address
+	    self.context        = None
+	    self.socket         = None
+	    self.server_thread  = None
+
+	    self.repulsive_cost  = repulsive_cost
+	    self.attractive_cost = attractive_cost
+
+	    # print( "Creating initial solution!" )
+	    self.initial_solution = initial_solution( self.graph, self.costs )
+	    self.current_solution = self.initial_solution
+	    # print( "Created initial solution!" )
+
+	    self.condition_object = threading.Event()
+	    self.interrupted      = True
+
+	    self.lock = threading.RLock()
+
+	def is_running( self ):
+	    with self.lock:
+	        return not self.interrupted
+
+	def start( self, ioThreads=1, timeout=10 ):
+	    with self.lock:
+	        if not self.is_running():
+	            self.interrupted   = False
+	            self.context       = zmq.Context.instance( ioThreads )
+	            self.socket        = self.context.socket( zmq.REP )
+	            self.socket.bind( self.address )
+
+	            target             = lambda : self._solve( timeout )
+	            self.server_thread = threading.Thread( target=target )
+	            self.server_thread.start()
+
+	def stop( self ):
+
+	    # with self.lock:
+	        if self.is_running():
+	            # print("stopping server!", self.context, self.socket, self.server_thread)
+	            self.interrupted = True
+	            self.condition_object.set()
+
+	            context = zmq.Context.instance( 1 )
+	            socket  = context.socket( zmq.REQ )
+	            socket.connect( self.address )
+	            socket.send_string( '' )
+	            socket.recv()
+	            self.condition_object.set()
+	            self.server_thread.join()
+	            self.socket.close()
+
+	            self.socket        = None
+	            self.context       = None
+	            self.server_thread = None
+
+	def _merge( self, id1, id2 ):
+		# print( "MERGE", id1, id2 )
+		set_costs_from_uv_ids( self.graph, self.costs, np.array([[id1, id2]]).astype(np.int32), self.attractive_cost )
+
+	def _detach( self, id ):
+		relevant_edges = [ edge for (node, edge) in self.graph.nodeAdjacency( id ) ]
+		self.costs[ relevant_edges ] = self.repulsive_cost
+
+	def _solution_to_message( self ):
+		# print('translatingsolutino to message', self.current_solution, self.current_solution.shape)
+		# message = bytes( 1 * 8 * self.current_solution.shape[0] )
+		# struct.pack_into( 'Q' * self.current_solution.shape[0], message, *self.current_solution )
+		# for k, v in enumerate( self.current_solution ):
+		# 	struct.pack_into( 'QQ', message, 2 * 8 * k, k, v )
+		# return message
+		return self.current_solution.tobytes()
+
+	def _solve( self, timeout ):
+		while self.is_running():
+			request = self.socket.recv()
+			length  = len( request )
+			# print( 'received message:', request, length )
+			with self.lock:
+				# print( 'preparing to send message!')
+				if length > 0:
+					offset  = 0
+					while offset < length:
+						action_type = struct.unpack_from( 'Q', request, offset=offset )[0]
+						# print( "ACTION TYPE", action_type )
+						offset += 8
+						if action_type == 0:
+							pass
+						elif action_type == 1:
+							id1, id2 = struct.unpack_from( 'QQ', request, offset=offset )
+							# print( "WTH", id1, id2, offset )
+							self._merge( id1, id2 )
+							offset += 2 * 8
+						elif action_type == 2:
+							self._detach( *struct.unpack_from( 'Q', request, offset=offset ) )
+							offset += 1 * 8
+
+					self.current_solution = solve_multicut( self.graph, self.costs )
+
+				# print('sending message!')
+				self.socket.send( self._solution_to_message() )
+				# print( 'sent message!' )
+
+			# self.condition_object.wait( timeout=timeout )
 
 # this sets the corresponding values in costs in-place
 def set_costs_from_uv_ids(graph, costs, uv_pairs, values):
-    edge_ids = graph.findEdges(uv_pairs)
-    edge_ids = edge_ids[edge_ids != -1]
-    costs[edge_ids] = values[edge_ids != -1]
+    edge_ids        = graph.findEdges(uv_pairs)
+    valid_edges     = edge_ids != -1
+    edge_ids        = edge_ids[ valid_edges ]
+    # print( 'any valid edges', valid_edges, np.any( valid_edges ) )
+
+    costs[edge_ids] = values[ valid_edges ] if isinstance( values, (np.ndarray, list, tuple) ) else values
 
 
 def set_costs_from_cluster_ids(graph, costs, node_labeling, cluster_u, cluster_v, value):
@@ -23,9 +146,11 @@ def set_costs_from_cluster_ids(graph, costs, node_labeling, cluster_u, cluster_v
     set_costs_from_uv_ids(graph, costs, potential_uvs, value)
 
 
-def solver_backend():
-    # receive actions (merge/detach)
-    # adjust cost
-    # resolve
-    # send updated solution
-    pass
+def start_server( graph, costs, address, ioThreads=1, timeout=10, initial_solution=lambda graph, costs : solve_multicut( graph, costs ) ):
+    server = SolverServer(
+        graph=graph,
+        costs=costs,
+        address=address,
+        initial_solution=initial_solution )
+    server.start( ioThreads=ioThreads, timeout=timeout)
+    return server
--- test/test_interactive_backend.py
+++ test/test_interactive_backend.py
@@ -1,80 +1,114 @@
+import collections
+import ctypes
+import numpy as np
+import nifty
+import os
+import struct
+import time
 import unittest
 import vigra
-import numpy as np
-import os
+import zmq
 
 # hacky import
 import sys
 sys.path.append('..')
-from solver_backend import set_costs_from_uv_ids, learn_rf, preprocess_with_random_forest
+from solver_backend import set_costs_from_uv_ids, learn_rf, preprocess_with_random_forest, interactive_backend
+
+
+
+edges = (
+	( 1, 4, -10 ),
+	( 2, 3, +10 ),
+	( 2, 6, +10 ),
+	( 3, 4, -10 ),
+	( 3, 7, -10 ),
+	( 4, 5, +10 ),
+	( 4, 8, +10 ),
+	( 5, 9, +10 )
+	)
+
+original_labeling = [
+	0, 1, 2, 2, 4, 4, 2, 7, 4, 4
+	]
+
+def to_graph( edges ):
+	uvIds    = [ (edge[0], edge[1]) for edge in edges ]
+	costs    = [ edge[2] for edge in edges ]
+	node_ids = np.unique( uvIds + [(0,0)] )
+	graph = nifty.graph.UndirectedGraph( np.max( node_ids ) + 1 )
+	graph.insertEdges( uvIds )
+	return graph, costs
+
+def relabel_to_smallest_member( solution ):
+    d = collections.defaultdict( list )
+    for frag, seg in enumerate( solution ):
+        d[ seg ].append( frag )
+    minimum_id_segments = { seg : min( frags ) for (seg, frags) in d.items() }
+    return np.vectorize( minimum_id_segments.get )( solution )
 
 class TestInteractiveBackend(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        import zipfile
-        from subprocess import call
 
-        super(TestInteractiveBackend, cls).setUpClass()
+    def test_server(self):
+        print("Running test server...")
+        graph, costs = to_graph( edges )
+        timeout    = 10
+        address = "inproc://mc-solver"
 
-        # download the test data
-        url = 'https://www.dropbox.com/s/l1tgzlim8h1pb7w/test_data_anisotropic.zip?dl=0'
-        data_file = 'data.zip'
-        # FIXME good old wget still does the job done better than any python lib I know....
-        call(['wget', '-O', data_file, url])
-        with zipfile.ZipFile(data_file) as f:
-            f.extractall('.')
-        os.remove(data_file)
+        context = zmq.Context.instance( 1 )
+        socket  = context.socket( zmq.REQ )
+        socket.connect( address )
 
-        # learn random forest
-        learn_rf(
-            ['./data/raw.h5'], ['data'],
-            ['./data/seg.h5'], ['data'],
-            ['./data/gt.h5'], ['data'],
-            './data/rf.pkl', 100
-        )
+        print ("Starting server!" )
 
-    # remove test data
-    @classmethod
-    def tearDownClass(cls):
-        from shutil import rmtree
-        super(TestInteractiveBackend, cls).tearDownClass()
-        rmtree('./data')
+        server  = interactive_backend.start_server( graph, costs, address, ioThreads=1, timeout=timeout )
 
-    def get_data_path(self):
-        return {
-            'seg': './data/seg.h5',
-            'raw': './data/raw.h5',
-            'pmap': './data/pmap.h5',
-            'rf':  './data/rf.pkl'
-        }
+        print( "Started server!", server.is_running() )
 
-    def test_set_costs(self):
-        inputs = self.get_data_path()
-        rag, costs = preprocess_with_random_forest(
-            inputs['raw'], 'data',
-            inputs['seg'], 'data',
-            inputs['rf'], 8
-        )
+        self.assertTrue( server.is_running() )
 
-        # sample some random uv-ids
-        uv_ids = rag.uvIds()
-        len_sample = int(.1*len(uv_ids))
-        indices = np.random.permutation(len(uv_ids))[:len_sample]
-        sampled_uvs = uv_ids[indices]
+        # get solution from no action
+        print( "Check initial solution!" )
+        socket.send_string( '' )
+        initial_solution = np.frombuffer( socket.recv(), dtype=np.uint64 )
 
-        # sample some random uv-pairs
-        sampled_uvs = np.concatenate(
-            [sampled_uvs, np.random.randint(0, rag.numberOfNodes - 1, size=(len_sample, 2))],
-            axis=0
-        )
+        self.assertTrue( np.all( initial_solution == server.current_solution ) )
+        self.assertTrue( np.all( relabel_to_smallest_member( initial_solution ) == original_labeling ) )
 
-        set_costs_from_uv_ids(rag, costs, sampled_uvs, 0.)
+        # send merge and evaluate
+        print( "Check merge!" )
+        id1 = 7
+        id2 = 3
+        socket.send( _merge( id1, id2  ) )
+        solution = np.frombuffer( socket.recv(), dtype=np.uint64 )
+        self.assertTrue( np.all( relabel_to_smallest_member( solution ) == np.array( [ 0, 1, 2, 2, 4, 4, 2, 2, 4, 4 ] ) ) )
 
-        edge_ids = rag.findEdges(sampled_uvs)
-        edge_ids = edge_ids[edge_ids != -1]
-        self.assertTrue((costs[edge_ids] == 0.).all())
+        # send detach and evaluate
+        print( "Check detach!" )
+        frag_id = 4
+        socket.send( _detach( frag_id ) )
+        solution = np.frombuffer( socket.recv(), dtype=np.uint64 )
+        self.assertTrue( np.all( relabel_to_smallest_member( solution ) == np.array( [ 0, 1, 2, 2, 4, 5, 2, 2, 8, 5 ] ) ) )
 
+        socket.close()
+
+        # time.sleep(seconds)
+        time.sleep( timeout * 2 * 1e-3 )
+        server.stop()
+        self.assertFalse( server.is_running() )
+        print( "Stopped server!" )
+
+def _np_arr_to_graph( arr ):
+	g = nifty.graph.UndirectedGraph( np.unique( arr ).size )
+	g.deserialize( arr )
+	return g
+
+def _merge( id1, id2 ):
+    # buf = ctypes.create_string_buffer( 1 * 4 + 2 * 8 )
+    return struct.pack( 'QQQ', 1, id1, id2 )
+
+def _detach( frag_id ):
+    return struct.pack( 'QQ', 2, frag_id )
 
 if __name__ == '__main__':
     unittest.main()
