7d3dce1305fd7bac7fa46d6e481aef7cd3334f80
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2017-09-07 13:51:33 -0400

    Use edge weights for multicut weights

--- solver_server_rf.py
+++ solver_server_rf.py
@@ -18,12 +18,14 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument( '--graph'          , '-g', default='/data/hanslovskyp/constantin-example-data/data/mc-graph.npy' )
 	parser.add_argument( '--costs'          , '-c', default='/data/hanslovskyp/constantin-example-data/data/mc-costs.npy' )
+	parser.add_argument( '--weights'        , '-w', default='/data/hanslovskyp/constantin-example-data/data/edge-weights.npy' )
 	parser.add_argument( '--features'       , '-f', default='/data/hanslovskyp/constantin-example-data/data/edge-features.npy' )
 	parser.add_argument( '--address'        , '-a', default='ipc:///tmp/mc-solver' )
 	parser.add_argument( '--logging-config' , '-l', default='/home/hanslovskyp/workspace/bigcat-future/interactive_solver_backend/logger.yaml' )
 
 	args    = parser.parse_args()
 	costs   = np.load( args.costs, allow_pickle=False )
+	weights = np.load( args.weights, allow_pickle=False )
 	address = args.address
 	graph   = nifty.graph.UndirectedGraph()
 
@@ -47,7 +49,7 @@ if __name__ == "__main__":
 
 	edge_features  = np.load( args.features, allow_pickle=False )
 	costs          = np.zeros( ( graph.numberOfEdges, ), dtype=np.float64 )
-	action_handler = solver_backend.TrainRandomForestFromAction( graph, costs,edge_features=edge_features )
+	action_handler = solver_backend.TrainRandomForestFromAction( graph, costs, edge_features=edge_features, edge_weights=weights )
 	server         = solver_backend.SolverServer( address, action_handler=action_handler )
 	server.start()
 
